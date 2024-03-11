ea273061fedf08e3fedc5b673845f83f79e144c5
Horwell <horwellmv@gmail.com>
2024-03-11 04:10:43 -0300

    Configura temas de color, container, clases css globales y cdn fontAwesome.

--- landingPage/tailwind.config.js
+++ landingPage/tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "color-primario":"#01051e",
+        "color-primario-light":"#020726",
+        "color-primario-dark":"#010417e",
+        "color-segundario":"#ff7d3b",
+        "color-gris":"#333",
+        "color-white":"#fff",
+        "color-gota":"#a427df",
+      }
+    },
+    container:{
+      center: true,
+      padding:{
+        default:"20px",
+        md:"50px"
+      }
+    }
   },
   plugins: [],
 }
